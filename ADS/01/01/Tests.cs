393c718e1290590445187acb3c1d60a536ef249c
Alexey Esin <a.esin@kefirgames.com>
2023-10-22 11:26:19 +0300

    01 template

---
+++ ADS/01/01/Tests.cs
@@ -0,0 +1,18 @@
+﻿using AlgorithmsDataStructures;
+using NUnit.Framework;
+
+namespace _01
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var node = new Node(1);
+            var ll = new LinkedList();
+            ll.AddInTail(node);
+            Assert.True(ll.Count() == 1);
+        }
+    }
+}
