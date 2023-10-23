90f7d41fcb4c0cd3a555e2ea9b060a1a49807330
Alexey Esin <a.esin@kefirgames.com>
2023-10-23 17:00:09 +0300

    03 start

---
+++ ADS/03/03/Template.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class DynArray<T>
+    {
+        public T [] array;
+        public int count;
+        public int capacity;
+
+        public DynArray()
+        {
+            count = 0;
+            MakeArray(16);
+        }
+
+        public void MakeArray(int new_capacity)
+        {
+            // ваш код
+        }
+
+        public T GetItem(int index)
+        {
+            // ваш код
+            return default(T);
+        }
+
+        public void Append(T itm)
+        {
+            // ваш код
+        }
+
+        public void Insert(T itm, int index)
+        {
+            // ваш код
+        }
+
+        public void Remove(int index)
+        {
+            // ваш код
+        }
+
+    }
+}
