486c5b6ceb67892b84e594a33553546fa33d13bc
Alexey Esin <a.esin@kefirgames.com>
2023-10-24 15:17:22 +0300

    04 first

---
+++ ADS/04/04/Template.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Stack<T>
+    {
+        private readonly LinkedList<T> _stack = new LinkedList<T>();
+        public Stack()
+        {
+        } 
+
+        public int Size() 
+        {
+            return _stack.Count;
+        }
+
+        public T Pop()
+        {
+            var last = _stack.Last;
+            _stack.RemoveLast();
+            return last.Value;
+        }
+	  
+        public void Push(T val)
+        {
+            _stack.AddLast(val);
+        }
+
+        public T Peek()
+        {
+            return _stack.Last.Value;
+        }
+    }
+
+}
