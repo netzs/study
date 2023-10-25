347bf18fd6f52eece5787a32de5938fcd7963054
Alexey Esin <a.esin@kefirgames.com>
2023-10-25 13:59:29 +0300

    07 template

---
+++ ADS/07/07/Template.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Node<T>
+    {
+        public T value;
+        public Node<T> next, prev;
+
+        public Node(T _value)
+        {
+            value = _value;
+            next = null;
+            prev = null;
+        }
+    }
+
+    public class OrderedList<T>
+    {
+        public Node<T> head, tail;
+        private bool _ascending;
+
+        public OrderedList(bool asc)
+        {
+            head = null;
+            tail = null;
+            _ascending = asc;
+        }
+
+        public int Compare(T v1, T v2)
+        {
+            int result = 0;
+            if(typeof(T) == typeof(String))
+            {
+                // версия для лексикографического сравнения строк
+            }
+            else 
+            {
+                // универсальное сравнение
+            }
+      
+            return result;
+            // -1 если v1 < v2
+            // 0 если v1 == v2
+            // +1 если v1 > v2
+        }
+
+        public void Add(T value)
+        {
+            // автоматическая вставка value 
+            // в нужную позицию
+        }
+
+        public Node<T> Find(T val)
+        {
+            return null; // здесь будет ваш код
+        }
+
+        public void Delete(T val)
+        {
+            // здесь будет ваш код
+        }
+
+        public void Clear(bool asc)
+        {
+            _ascending = asc;
+            // здесь будет ваш код
+        }
+
+        public int Count()
+        {
+            return 0; // здесь будет ваш код подсчёта количества элементов в списке
+        }
+
+        List<Node<T>> GetAll() // выдать все элементы упорядоченного 
+            // списка в виде стандартного списка
+        {
+            List<Node<T>> r = new List<Node<T>>();
+            Node<T> node = head;
+            while(node != null)
+            {
+                r.Add(node);
+                node = node.next;
+            }
+            return r;
+        }
+    }
+ 
+}
