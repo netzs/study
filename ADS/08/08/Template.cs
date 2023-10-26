6e791e6b52c63ff1464b1ad75fa79d7066d4277e
Alexey Esin <a.esin@kefirgames.com>
2023-10-26 18:09:02 +0300

    08 finish

--- ADS/08/08/Template.cs
+++ ADS/08/08/Template.cs
@@ -3,47 +3,78 @@ using System.Collections.Generic;
 
 namespace AlgorithmsDataStructures
 {
-
     public class HashTable
     {
         public int size;
         public int step;
-        public string [] slots; 
+        public string[] slots;
+
+        private const int P1 = 514229;
+        private const int P2 = 9369319;
 
         public HashTable(int sz, int stp)
         {
             size = sz;
             step = stp;
             slots = new string[size];
-            for(int i=0; i<size; i++) slots[i] = null;
+            for (int i = 0; i < size; i++) slots[i] = null;
         }
 
         public int HashFun(string value)
-        {    
-            // всегда возвращает корректный индекс слота
-            return 0;
+        {
+            long sum = 0;
+            for (var index = 0; index < value.Length; index++)
+            {
+                char symbol = value[index];
+                sum = (sum + P1 * symbol + P2 * index) % size;
+            }
+
+            return (int) sum;
         }
 
         public int SeekSlot(string value)
         {
-            // находит индекс пустого слота для значения, или -1
+            var startIndex = HashFun(value);
+            var index = startIndex;
+            do
+            {
+                if (slots[index] == null)
+                {
+                    return index;
+                }
+
+                index = (index + step) % size;
+            } while (index != startIndex);
+
             return -1;
         }
 
         public int Put(string value)
         {
-            // записываем значение по хэш-функции
-         
-            // возвращается индекс слота или -1
-            // если из-за коллизий элемент не удаётся разместить 
-            return -1;
+            var index = SeekSlot(value);
+            if (index != -1)
+            {
+                slots[index] = value;
+            }
+
+            return index;
         }
 
         public int Find(string value)
         {
-            // находит индекс слота со значением, или -1
+            var startIndex = HashFun(value);
+            var index = startIndex;
+            do
+            {
+                if (slots[index] == value)
+                {
+                    return index;
+                }
+
+                index = (index + step) % size;
+            } while (index != startIndex);
+
             return -1;
         }
     }
- 
 }
--- ADS/08/08/Tests.cs
+++ ADS/08/08/Tests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using AlgorithmsDataStructures;
 using NUnit.Framework;
 
 namespace _08
@@ -9,7 +10,86 @@ namespace _08
         [Test]
         public void Test1()
         {
-            Assert.True(true);
+            var hash = new HashTable(17, 3);
+            Assert.True(hash.SeekSlot("123") >= 0 && hash.SeekSlot("123") < 17);
+            var k = hash.Put("a11");
+            var k1 = hash.Put("a11");
+            var k2 = hash.Put("a11");
+            var k3 = hash.Put("a11");
+            Assert.True(k != k1 && k1 != k2 && k2 != k3 && k3 != k && k != k2);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var hash = new HashTable(17, 3);
+            var set = new HashSet<int>();
+            for (int i = 0; i < 17; i++)
+            {
+                var k = hash.Put("a11");
+                set.Add(k);
+                Assert.True(k != -1 && k < 17);
+            }
+
+            Assert.True(set.Count == 17);
+
+            Assert.True(hash.Put("a11") == -1);
+            Assert.True(hash.Put("a") == -1);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var hash = new HashTable(17, 3);
+            for (int i = 0; i < 17; i++)
+            {
+                var k = hash.Put(i.ToString());
+                Assert.True(k != -1 && k < 17);
+            }
+
+            for (int i = 0; i < 17; i++)
+            {
+                var k = hash.Put(i.ToString());
+                Assert.True(k == -1);
+            }
+
+            var set = new HashSet<int>();
+            for (int i = 0; i < 17; i++)
+            {
+                var k = hash.Find(i.ToString());
+                Assert.True(k != -1 && k < 17);
+                set.Add(k);
+            }
+
+            Assert.True(hash.Find("18") == -1);
+
+            Assert.True(set.Count == 17);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var hash = new HashTable(123123, 123122);
+            for (int i = 0; i < 100000; i++)
+            {
+                var k = hash.Put(i.ToString());
+                Assert.True(k != -1 && k < 123123);
+            }
+        }
+        
+        [Test]
+        public void Test5()
+        {
+            var hash = new HashTable(17, 3);
+            var text0 = "";
+            var text1 = "b";
+            var text2 = "c";
+            var a0 = hash.Put(text0);
+            var a1 = hash.Put(text1);
+            var a2 = hash.Put(text2);
+            Assert.True(hash.Find(text0) == a0);
+            Assert.True(hash.Find(text1) == a1);
+            Assert.True(hash.Find(text2) == a2);
         }
     }
 }
