549f9d7f1bf089bfc068b352fb13e50e1b1eafbb
Alexey Esin <a.esin@kefirgames.com>
2023-10-26 13:34:56 +0300

    08 template

---
+++ ADS/08/08/Template.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class HashTable
+    {
+        public int size;
+        public int step;
+        public string [] slots; 
+
+        public HashTable(int sz, int stp)
+        {
+            size = sz;
+            step = stp;
+            slots = new string[size];
+            for(int i=0; i<size; i++) slots[i] = null;
+        }
+
+        public int HashFun(string value)
+        {    
+            // всегда возвращает корректный индекс слота
+            return 0;
+        }
+
+        public int SeekSlot(string value)
+        {
+            // находит индекс пустого слота для значения, или -1
+            return -1;
+        }
+
+        public int Put(string value)
+        {
+            // записываем значение по хэш-функции
+         
+            // возвращается индекс слота или -1
+            // если из-за коллизий элемент не удаётся разместить 
+            return -1;
+        }
+
+        public int Find(string value)
+        {
+            // находит индекс слота со значением, или -1
+            return -1;
+        }
+    }
+ 
+}
