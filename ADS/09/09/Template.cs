86df318d02985549af1790931c183040e8863ff4
Alexey Esin <a.esin@kefirgames.com>
2023-10-26 18:21:13 +0300

    09 template

---
+++ ADS/09/09/Template.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class NativeDictionary<T>
+    {
+        public int size;
+        public string [] slots;
+        public T [] values;
+
+        public NativeDictionary(int sz)
+        {
+            size = sz;
+            slots = new string[size];
+            values = new T[size];
+        }
+
+        public int HashFun(string key)
+        {
+            // всегда возвращает корректный индекс слота
+            return 0;
+        }
+
+        public bool IsKey(string key)
+        {
+            // возвращает true если ключ имеется,
+            // иначе false
+            return false;
+        }
+
+        public void Put(string key, T value)
+        {
+            // гарантированно записываем 
+            // значение value по ключу key
+        }
+
+        public T Get(string key)
+        {
+            // возвращает value для key, 
+            // или null если ключ не найден
+            return default(T);
+        }
+    } 
+}
