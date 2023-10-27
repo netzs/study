6aec1a73092bbe4050cd58e795d3a79c49be4fd6
Alexey Esin <a.esin@kefirgames.com>
2023-10-27 20:01:38 +0300

    09 finish

--- ADS/09/09/Template.cs
+++ ADS/09/09/Template.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 
 namespace AlgorithmsDataStructures
 {
-
     public class NativeDictionary<T>
     {
         public int size;
-        public string [] slots;
-        public T [] values;
+        public string[] slots;
+        public T[] values;
+
+        private const int P1 = 514229;
+        private const int P2 = 9369319;
+
+        public int step = 3;
 
         public NativeDictionary(int sz)
         {
@@ -19,28 +23,77 @@ namespace AlgorithmsDataStructures
 
         public int HashFun(string key)
         {
-            // всегда возвращает корректный индекс слота
-            return 0;
+            long sum = 0;
+            for (var index = 0; index < key.Length; index++)
+            {
+                char symbol = key[index];
+                sum = (sum + P1 * symbol + P2 * index) % size;
+            }
+
+            return (int) sum;
         }
 
         public bool IsKey(string key)
         {
-            // возвращает true если ключ имеется,
-            // иначе false
-            return false;
+            var index = FindKey(key);
+            return index != -1;
         }
 
         public void Put(string key, T value)
         {
-            // гарантированно записываем 
-            // значение value по ключу key
+            var index = FindKeyOrEmpty(key);
+            if (slots[index] == null)
+            {
+                slots[index] = key;
+            }
+
+            values[index] = value;
         }
 
         public T Get(string key)
         {
-            // возвращает value для key, 
-            // или null если ключ не найден
-            return default(T);
+            var index = FindKey(key);
+
+            return index == -1 ? default(T) : values[index];
         }
-    } 
+
+        private int FindKey(string value)
+        {
+            var startIndex = HashFun(value);
+            var index = startIndex;
+            do
+            {
+                if (slots[index] == null)
+                {
+                    return -1;
+                }
+
+                if (slots[index] == value)
+                {
+                    return index;
+                }
+
+                index = (index + step) % size;
+            } while (index != startIndex);
+
+            return -1;
+        }
+
+        private int FindKeyOrEmpty(string value)
+        {
+            var startIndex = HashFun(value);
+            var index = startIndex;
+            do
+            {
+                if (slots[index] == value || slots[index] == null)
+                {
+                    return index;
+                }
+
+                index = (index + step) % size;
+            } while (index != startIndex);
+
+            return -1;
+        }
+    }
 }
--- ADS/09/09/Tests.cs
+++ ADS/09/09/Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AlgorithmsDataStructures;
 using NUnit.Framework;
 
 namespace _09
@@ -9,7 +10,37 @@ namespace _09
         [Test]
         public void Test1()
         {
-            Assert.True(true);
+            var dict = new NativeDictionary<int>(17);
+            dict.Put("a", 1);
+            Assert.True(dict.IsKey("a"));
+            Assert.True(dict.Get("a") == 1);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var dict = new NativeDictionary<int>(27);
+            for (var i = 'a'; i < 'z'; i++)
+            {
+                dict.Put("" + i, i);
+            }
+
+            for (var i = 'a'; i < 'z'; i++)
+            {
+                Assert.True(dict.Get("" + i) == i);
+            }
+            
+            for (var i = 'a'; i < 'z'; i++)
+            {
+                dict.Put("" + i, i + 100);
+                Assert.True(dict.IsKey("" + i));
+                Assert.False(dict.IsKey("-" + i));
+            }
+            
+            for (var i = 'a'; i < 'z'; i++)
+            {
+                Assert.True(dict.Get("" + i) == i + 100);
+            }
         }
     }
 }
