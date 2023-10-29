5ab1a58f25fe94daef4b7dd512b99e68a7f55944
Alexey Esin <a.esin@kefirgames.com>
2023-10-29 21:05:36 +0300

    11 finish

---
+++ ADS/11/11/Template.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace AlgorithmsDataStructures
+{
+    public class BloomFilter
+    {
+        public int filter_len;
+        private int[] data;
+
+        public BloomFilter(int f_len)
+        {
+            filter_len = f_len;
+            data = new int[(f_len + 31) / 32];
+        }
+
+        public int Hash1(string str1)
+        {
+            return Hash(str1, 17);
+        }
+
+        public int Hash2(string str1)
+        {
+            return Hash(str1, 223);
+        }
+
+        private int Hash(string str, int random)
+        {
+            var result = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                int code = str[i];
+                result = (result * random + code) % filter_len;
+            }
+
+            return result;
+        }
+
+        public void Add(string str1)
+        {
+            AddBit(Hash1(str1));
+            AddBit(Hash2(str1));
+        }
+
+        private void AddBit(int index)
+        {
+            data[index / 32] |= 1 << (index % 32);
+        }
+        
+        private bool GetBit(int index)
+        {
+            return (data[index / 32] & (1 << (index % 32))) != 0;
+        }
+
+        public bool IsValue(string str1)
+        {
+            return GetBit(Hash1(str1)) && GetBit(Hash2(str1));
+        }
+    }
+}
---
+++ ADS/11/11/Tests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using AlgorithmsDataStructures;
+using NUnit.Framework;
+
+namespace _11
+{
+    [TestFixture]
+    public class Tests
+    {
+        private string[] _data = new[]
+        {
+            "0123456789",
+            "1234567890",
+            "2345678901",
+            "3456789012",
+            "4567890123",
+            "5678901234",
+            "6789012345",
+            "7890123456",
+            "8901234567",
+            "9012345678",
+        };
+        
+        [Test]
+        public void TestHash()
+        {
+            var blum = new BloomFilter(128);
+            for (int i = 0; i < 1000000; i++)
+            {
+                Assert.True(blum.Hash1(GetRandom()) < 128);
+                Assert.True(blum.Hash2(GetRandom()) < 128);
+            }
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var blum = new BloomFilter(32);
+            blum.Add(_data[0]);
+            Assert.True(blum.IsValue(_data[0]));
+            for (int i = 1; i < 10; i++)
+            {
+                blum.Add(_data[i]);
+                Assert.True(blum.IsValue(_data[i]));
+            }
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var blum = new BloomFilter(29);
+            var list = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                var str = GetRandom();
+                list.Add(str);
+                blum.Add(str);
+                Assert.True(blum.IsValue(str));
+            }
+
+            foreach (var str in list)
+            {
+                Assert.True(blum.IsValue(str));
+            }
+
+            var count = 0;
+            var testCount = 100000;
+            for (int i = 0; i < testCount; i++)
+            {
+                count += blum.IsValue(GetRandom()) ? 1 : 0;
+            }
+
+            Assert.True(count < testCount * 35 / 100);
+        }
+
+        private Random _random = new Random();
+
+        private string GetRandom()
+        {
+            StringBuilder s = new StringBuilder();
+            for (var i = 0; i < 20; i++)
+            {
+                s.Append((char) ((_random.Next() % 26) + 'a'));
+            }
+
+            return s.ToString();
+        }
+    }
+}
