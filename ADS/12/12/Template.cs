a17a3dcf79230be9ef9aa1c3b17c6c0f45c3a2b0
Alexey Esin <a.esin@kefirgames.com>
2023-11-01 21:15:11 +0300

    12

---
+++ ADS/12/12/Template.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmsDataStructures
+{
+    public class NativeCache<T>
+    {
+        public int size;
+        public String[] slots;
+        public T[] values;
+        public int[] hits;
+
+        public NativeCache(int sz)
+        {
+            size = sz;
+            slots = new string[size];
+            values = new T[size];
+            hits = new int[size];
+        }
+
+        private const int P1 = 514229;
+        private const int P2 = 9369319;
+
+        public int step = 1;
+
+        public int HashFun(string key)
+        {
+            long sum = 0;
+            for (var index = 0; index < key.Length; index++)
+            {
+                char symbol = key[index];
+                sum = (sum + P1 * symbol + P2 * index) % size;
+            }
+
+            return (int) sum;
+        }
+
+        public bool IsKey(string key)
+        {
+            var index = FindKey(key);
+            return index != -1;
+        }
+
+        public void Put(string key, T value)
+        {
+            var index = FindKeyOrEmpty(key);
+            if (index == -1)
+            {
+                index = GetReleaseSlotIndex();
+            }
+            slots[index] = key;
+            values[index] = value;
+            hits[index] = 0;
+        }
+
+        private int GetReleaseSlotIndex()
+        {
+            var minIndex = 0;
+            var minValue = hits[minIndex];
+            for (var index = 0; index < hits.Length; index++)
+            {
+                if (hits[index] < minValue)
+                {
+                    minIndex = index;
+                    minValue = hits[index];
+                }
+            }
+
+            return minIndex;
+        }
+
+        public T Get(string key)
+        {
+            var index = FindKey(key);
+            if (index == -1)
+            {
+                return default;
+            }
+
+            hits[index]++;
+            return values[index];
+        }
+
+        private int FindKey(string value)
+        {
+            var startIndex = HashFun(value);
+            var index = startIndex;
+            do
+            {
+                if (slots[index] == null)
+                {
+                    return -1;
+                }
+
+                if (slots[index] == value)
+                {
+                    return index;
+                }
+
+                index = (index + step) % size;
+            } while (index != startIndex);
+
+            return -1;
+        }
+
+        private int FindKeyOrEmpty(string value)
+        {
+            var startIndex = HashFun(value);
+            var index = startIndex;
+            do
+            {
+                if (slots[index] == value || slots[index] == null)
+                {
+                    return index;
+                }
+
+                index = (index + step) % size;
+            } while (index != startIndex);
+
+            return -1;
+        }
+    }
+}
---
+++ ADS/12/12/Tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using AlgorithmsDataStructures;
+using NUnit.Framework;
+
+namespace _12
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test0()
+        {
+            var cache = new NativeCache<int>(2);
+            cache.Put("a", 1);
+            cache.Put("b", 2);
+            cache.Get("b");
+            cache.Get("b");
+            cache.Get("a");
+            cache.Put("c", 3);
+            Assert.False(cache.IsKey("a"));
+            Assert.True(cache.Get("b") == 2);
+            Assert.True(cache.Get("c") == 3);
+            Assert.True(cache.Get("c") == 3);
+            Assert.True(cache.Get("c") == 3);
+            Assert.True(cache.Get("c") == 3);
+            cache.Put("d", 4);
+            cache.Put("c", 3);
+            Assert.False(cache.IsKey("b"));
+            Assert.True(cache.IsKey("c"));
+            Assert.True(cache.IsKey("d"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var cache = new NativeCache<int>(1);
+            cache.Put("a", 1);
+            Assert.True(cache.hits[0] == 0);
+            cache.IsKey("a");
+            Assert.True(cache.hits[0] == 0);
+            cache.Get("a");
+            Assert.True(cache.hits[0] == 1);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var cache = new NativeCache<int>(3);
+            for (var i = 0; i < 3; i++)
+            {
+                cache.Put(i.ToString(), i);
+            }
+
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = i; j < 3; j++)
+                {
+                    cache.Get(i.ToString());
+                }
+            }
+
+            cache.Put("4", 4);
+            Assert.True(cache.IsKey("4"));
+            Assert.False(cache.IsKey("3"));
+        }
+
+    }
+}
