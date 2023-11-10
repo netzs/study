16e00dd267a2443ea96797e1ee0fac5a7d261152
Alexey Esin <a.esin@kefirgames.com>
2023-11-10 23:00:50 +0300

    CC 04

---
+++ CC/04/04.cs
@@ -0,0 +1,29 @@
+sum - sumViewPosition
+// Вычисление среднестатистического позиций.
+raycastNum - raycastHitCount
+// Количество пересечений при рейкасте.
+deformation2 - secondStepDeformation
+// Поворот кости меша, в преобразовании в несколько шагов.
+list - postfixAssetPathDict
+// Переменная называлась list, хотя была словарь.
+safeDoorString - safeDoorExportText
+// String не означает ничего кроме типа.
+values - csvLineSplit
+// Разбиение строки csv файла по ";".
+l - lockPacketCount
+// Переменная используется в арифметических операциях.
+totalTime - timeTotal
+// Общее время анимации.
+variable - keyButtonParam
+// Строка выглядит так var variable = _localizedString[ValueKey] as Variable<string>; выделение переменных для подстановки подстроки в строку, но выглядит как сборник неправильного именования.
+// Переменная для отображения клавиши клавиатуры.
+inputValue - inputLookValue
+// Величина смещение мыши в действии от пользователя "озираться по сторонам".
+timeDiff - timeDiffSec / deltaTime - deltaTimeMilisec
+// В одном методе были разные измерения времени.
+result - linkResult
+// Переменная не является той, которая должна вернуться через return. Результат отправки пакета.
+
+
+
+
