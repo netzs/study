3eb86f76bb23c0ce32104c2b0e7301f0888973ae
Alexey Esin <a.esin@kefirgames.com>
2023-11-14 23:37:46 +0300

    cc 06

---
+++ CC/06/06.cs
@@ -0,0 +1,39 @@
+CreateMarkerPoint - CalcProjectionPoint
+// Расчет проекции двухмерной точки на трехмерную карту. Не соответствие название и того, что делает функция.
+
+CorrectWater - GetPointOutsideWater
+// Расчет точки вне воды.
+
+GetMapPoint - CalculateMapPoint
+// Возвращает позицию карты.
+
+HasCarEntityCacheModel - CheckChangedInVehicleId
+// Изменился ли транспорт, в котором находится игрок.
+
+SetBecomeRobberAnim - PlayBecomeRobberAnim
+// Запустить анимацию.
+
+SetMinimap - SetMinimapTexture
+// Установить текстуру для миникарты. Недостаточно информации в названии.
+
+BlockCalculate - CheckRayBlocked
+// Проверка есть при препятствие на пути луча.
+
+SetKeyboard - SetKeyboardModeView
+// Установить режим отображения для работы с клавиатуры.
+
+GetIsDistanceBetweenCharactersCorrect - CheckCharactersDistance
+// Корректно ли расстояние между игроками. Избыточное использование слов.
+
+WhetherAnySkinWasClicked - CheckSkinButtonsClicked
+// Лишние слова. Нажата ли на какая-то кнопка выбора скина.
+
+GetIsPlaying - IsFootstepSoundPlaying
+// Проигрывается ли звук шага.
+
+CitizenToCop - OpenCopJobDialog
+// Нет глагола, непонятно что делает метод.
+
+
+
+
