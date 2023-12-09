a3bbbd31ec69af486afdb7ff29317b582db325fc
Alexey Esin <a.esin@kefirgames.com>
2023-12-09 17:12:14 +0300

    ADS2 05

---
+++ ADS2/05/05/BalancedBST.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public static class BalancedBST
+    {
+        public static int[] GenerateBBSTArray(int[] a)
+        {
+            var c = new int[a.Length];
+            for (var i = 0; i < a.Length; i++)
+            {
+                c[i] = a[i];
+            }
+            Array.Sort(c);
+            var result = new int[a.Length];
+            if (result.Length > 0)
+            {
+                Round(c, result, 0, a.Length - 1, 0);
+            }
+
+            return result;
+        }
+
+        private static void Round(int[] data, int[] result, int left, int right, int index)
+        {
+            int center = (right + left) / 2;
+            result[index] = data[center];
+            if (right == left)
+            {
+                return;
+            }
+            
+            Round(data, result, left, center - 1, index * 2 + 1);
+            Round(data, result, center + 1, right, index * 2 + 2);
+        }
+    }
+}
---
+++ ADS2/05/05/Tests.cs
@@ -0,0 +1,30 @@
+﻿using AlgorithmsDataStructures2;
+using NUnit.Framework;
+
+namespace _05
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Check(BalancedBST.GenerateBBSTArray(new[] {3, 2, 1}), new[] {2, 1, 3});
+            Check(BalancedBST.GenerateBBSTArray(new[] {2, 1, 3}), new[] {2, 1, 3});
+            Check(BalancedBST.GenerateBBSTArray(new[] {3, 3, 3}), new[] {3, 3, 3});
+            Check(BalancedBST.GenerateBBSTArray(new[] {300, 300, 3}), new[] {300, 3, 300});
+            Check(BalancedBST.GenerateBBSTArray(new[] {1}), new[] {1});
+            Check(BalancedBST.GenerateBBSTArray(new[] {1, 3, 2, 7, 6, 5, 4}), new[] {4, 2, 6, 1, 3, 5, 7});
+            Check(BalancedBST.GenerateBBSTArray(new int [] {}), new int [] {});
+        }
+
+        private void Check(int[] a, int[] b)
+        {
+            Assert.True(a.Length == b.Length);
+            for (int i = 0; i < a.Length; i++)
+            {
+                Assert.True(a[i] == b[i]);
+            }
+        }
+    }
+}
