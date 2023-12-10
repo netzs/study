8a761da296c55d47b7004288b2cf58b23158ef7f
Alexey Esin <a.esin@kefirgames.com>
2023-12-10 16:16:37 +0300

    ASD2 06

---
+++ ADS2/06/06/BalancedBST.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Activation;
+
+namespace AlgorithmsDataStructures2
+{
+    public class BSTNode
+    {
+        public int NodeKey; // ключ узла
+        public BSTNode Parent; // родитель или null для корня
+        public BSTNode LeftChild; // левый потомок
+        public BSTNode RightChild; // правый потомок	
+        public int Level; // глубина узла
+
+        public BSTNode(int key, BSTNode parent)
+        {
+            NodeKey = key;
+            Parent = parent;
+            LeftChild = null;
+            RightChild = null;
+        }
+    }
+
+
+    public class BalancedBST
+    {
+        public BSTNode Root; // корень дерева
+
+        public BalancedBST()
+        {
+            Root = null;
+        }
+
+        public void GenerateTree(int[] a)
+        {
+            int[] c = new int [a.Length];
+            Array.Copy(a, c, a.Length);
+            Array.Sort(c);
+            Root = CreateNode(c, 0, c.Length - 1, 0, null);
+        }
+
+        private BSTNode CreateNode(int[] data, int left, int right, int level, BSTNode parent)
+        {
+            if (left > right)
+            {
+                return null;
+            }
+
+            var center = (left + right) / 2;
+            var node = new BSTNode(data[center], parent)
+            {
+                Level = level
+            };
+
+            node.LeftChild = CreateNode(data, left, center - 1, level + 1, node);
+            node.RightChild = CreateNode(data, center + 1, right, level + 1, node);
+            return node;
+        }
+
+        public bool IsBalanced(BSTNode root_node)
+        {
+            return GetIsBalancedAndDepth(root_node).Item1;
+        }
+
+        private (bool, int) GetIsBalancedAndDepth(BSTNode root_node)
+        {
+            (bool leftBalanced, int leftDepth) = root_node.LeftChild == null ? (true, root_node.Level) : GetIsBalancedAndDepth(root_node.LeftChild);
+            (bool rightBalanced, int rightDepth) = root_node.RightChild == null ? (true, root_node.Level) : GetIsBalancedAndDepth(root_node.RightChild);
+
+            return (leftBalanced && rightBalanced && Math.Abs(leftDepth - rightDepth) <= 1, Math.Max(leftDepth, rightDepth));
+        }
+    }
+}
---
+++ ADS2/06/06/Tests.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using AlgorithmsDataStructures2;
+using NUnit.Framework;
+
+namespace _06
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var tree = new BalancedBST();
+            tree.GenerateTree(new[] {1});
+            Assert.True(tree.IsBalanced(tree.Root));
+            Check(tree.Root, new HashSet<int>() {1}, 0);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var tree = new BalancedBST();
+            var ar = new[] {1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27};
+            tree.GenerateTree(ar);
+            Check(tree.Root, new HashSet<int>(ar), 0);
+            Assert.True(tree.IsBalanced(tree.Root));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var tree = new BalancedBST();
+            var ar = new[] {3, 2, 1, 4};
+            tree.GenerateTree(ar);
+            Check(tree.Root, new HashSet<int>(ar), 0);
+            Check(tree.Root.LeftChild, new HashSet<int>() {1}, 1);
+            Check(tree.Root.RightChild, new HashSet<int>() {3, 4}, 1);
+            Assert.True(tree.IsBalanced(tree.Root));
+            
+        }
+        
+        [Test]
+        public void Test4()
+        {
+            var tree = new BalancedBST();
+            var ar = new[] {3, 2, 1, 4};
+            tree.GenerateTree(ar);
+            tree.Root.LeftChild = null;
+            Assert.False(tree.IsBalanced(tree.Root));
+            Assert.True(tree.IsBalanced(tree.Root.RightChild));
+            
+        }
+
+        private void Check(BSTNode node, HashSet<int> hashSet, int level)
+        {
+            Assert.True(hashSet.Contains(node.NodeKey));
+            Assert.True(node.Level == level);
+            hashSet.Remove(node.NodeKey);
+            if (node.LeftChild != null)
+            {
+                Assert.True(node.LeftChild.NodeKey < node.NodeKey);
+                Assert.True(node.LeftChild.Parent == node);
+                Check(node.LeftChild, hashSet, level + 1);
+            }
+
+            if (node.RightChild != null)
+            {
+                Assert.True(node.NodeKey <= node.RightChild.NodeKey);
+                Assert.True(node.RightChild.Parent == node);
+                Check(node.RightChild, hashSet, level + 1);
+            }
+        }
+    }
+}
