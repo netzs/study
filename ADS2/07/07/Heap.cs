19c67e624ff6623be03774b95da8ce53ce0e71dc
Alexey Esin <a.esin@kefirgames.com>
2023-12-11 22:03:12 +0300

    ASD2 07

---
+++ ADS2/07/07/Heap.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+
+namespace AlgorithmsDataStructures2
+{
+    public class Heap
+    {
+        public int[] HeapArray; // хранит неотрицательные числа-ключи
+        private int _size;
+
+        public Heap()
+        {
+            HeapArray = null;
+        }
+
+        public void MakeHeap(int[] a, int depth)
+        {
+            _size = a.Length;
+            HeapArray = new int [(1 << (depth + 1)) - 1];
+            Array.Copy(a, HeapArray, _size);
+            Array.Sort(HeapArray, 0, _size);
+            Array.Reverse(HeapArray, 0, _size);
+        }
+
+        public int GetMax()
+        {
+            if (_size == 0)
+            {
+                return -1;
+            }
+
+            _size--;
+            var maxValue = HeapArray[0];
+            HeapArray[0] = HeapArray[_size];
+            Down(0);
+
+            return maxValue;
+        }
+
+        private void Down(int index)
+        {
+            var maxIndex = GetMaxIndex(index);
+            if (maxIndex != index)
+            {
+                Swap(index, maxIndex);
+                Down(maxIndex);
+            }
+        }
+
+        private int GetMaxIndex(int index)
+        {
+            int leftIndex = index * 2 + 1;
+            int rightIndex = index * 2 + 2;
+
+            if (rightIndex < _size && HeapArray[leftIndex] < HeapArray[rightIndex] && HeapArray[index] < HeapArray[rightIndex])
+            {
+                return rightIndex;
+            }
+
+            if (leftIndex < _size && HeapArray[index] < HeapArray[leftIndex])
+            {
+                return leftIndex;
+            }
+
+            return index;
+        }
+
+        private void Swap(int index1, int index2)
+        {
+            (HeapArray[index1], HeapArray[index2]) = (HeapArray[index2], HeapArray[index1]);
+        }
+
+        public bool Add(int key)
+        {
+            if (_size == HeapArray.Length)
+            {
+                return false;
+            }
+
+            HeapArray[_size] = key;
+            Up(_size);
+            _size++;
+
+            return true;
+        }
+
+        private void Up(int index)
+        {
+            if (index == 0)
+            {
+                return;
+            }
+
+            var parentIndex = (index - 1) / 2;
+            if (HeapArray[parentIndex] < HeapArray[index])
+            {
+                Swap(parentIndex, index);
+                Up(parentIndex);
+            }
+        }
+    }
+}
---
+++ ADS2/07/07/Tests.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using AlgorithmsDataStructures2;
+using NUnit.Framework;
+
+namespace _07
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestCreate()
+        {
+            var heap = new Heap();
+            heap.MakeHeap(new int[] { }, 2);
+            Assert.True(heap.HeapArray.Length == 7);
+            heap.MakeHeap(new int[] { }, 3);
+            Assert.True(heap.HeapArray.Length == 15);
+            heap.MakeHeap(new int[] {1, 2, 3, 4, 5}, 2);
+            Check(heap, new int[] {5, 4, 3, 2, 1});
+        }
+
+        [Test]
+        public void TestCreate2()
+        {
+            var heap = new Heap();
+            heap.MakeHeap(new int[] {1, 2, 3, 4, 5}, 2);
+            Check(heap, new int[] {5, 4, 3, 2, 1});
+            heap.MakeHeap(new int[] { }, 2);
+            for (int i = 0; i < 7; i++)
+            {
+                Assert.True(heap.Add(1));
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                Assert.False(heap.Add(1));
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                Assert.True(heap.GetMax() == 1);
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                Assert.True(heap.GetMax() == -1);
+            }
+        }
+
+        [Test]
+        public void TestRnd1()
+        {
+            var heap = new Heap();
+            heap.MakeHeap(new int[] { }, 10);
+            var random = new Random();
+
+            var rnds = new int[1000];
+            for (int i = 0; i < 1000; i++)
+            {
+                rnds[i] = -random.Next(0, 10000);
+                heap.Add(-rnds[i]);
+            }
+
+            Array.Sort(rnds);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Assert.True(heap.GetMax() == -rnds[i]);
+            }
+        }
+
+        [Test]
+        public void TestRnd2()
+        {
+            var heap = new Heap();
+            var random = new Random();
+
+            var rnds = new int[1000];
+            for (int i = 0; i < 1000; i++)
+            {
+                rnds[i] = random.Next(0, 10000);
+            }
+
+            heap.MakeHeap(rnds, 10);
+
+            Array.Sort(rnds);
+            Array.Reverse(rnds);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Assert.True(heap.GetMax() == rnds[i]);
+            }
+        }
+
+        [Test]
+        public void TestRnd3()
+        {
+            var heap = new Heap();
+            var random = new Random();
+
+            var rnds = new int[1000];
+            for (int i = 0; i < 1000; i++)
+            {
+                rnds[i] = random.Next(9, 10000);
+            }
+
+            var rnd2 = new int[500];
+            Array.Copy(rnds, rnd2, 500);
+            heap.MakeHeap(rnd2, 9);
+
+            for (int i = 500; i < 1000; i++)
+            {
+                Assert.True(heap.Add(rnds[i]));
+            }
+
+            for (int i = 0; i < 23; i++)
+            {
+                Assert.True(heap.Add(1));
+            }
+
+            Assert.False(heap.Add(1));
+            
+            Array.Sort(rnds);
+            Array.Reverse(rnds);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Assert.True(heap.GetMax() == rnds[i]);
+            }
+        }
+
+        [Test]
+        public void TestAdd()
+        {
+            var heap = new Heap();
+            heap.MakeHeap(new int[] {}, 3);
+            heap.Add(10);
+            Assert.True(heap.HeapArray[0] == 10);
+            heap.Add(11);
+            Assert.True(heap.HeapArray[0] == 11);
+            Assert.True(heap.HeapArray[1] == 10);
+            heap.Add(12);
+            Assert.True(heap.HeapArray[0] == 12);
+            Assert.True(heap.HeapArray[1] == 10);
+            Assert.True(heap.HeapArray[2] == 11);
+            heap.Add(5);
+            Assert.True(heap.HeapArray[0] == 12);
+            Assert.True(heap.HeapArray[1] == 10);
+            Assert.True(heap.HeapArray[2] == 11);
+            Assert.True(heap.HeapArray[3] == 5);
+            heap.Add(20);
+            Assert.True(heap.HeapArray[0] == 20);
+            Assert.True(heap.HeapArray[1] == 12);
+            Assert.True(heap.HeapArray[2] == 11);
+            Assert.True(heap.HeapArray[3] == 5);
+            Assert.True(heap.HeapArray[4] == 10);
+            Assert.True(heap.GetMax() == 20);
+            Assert.True(heap.HeapArray[0] == 12);
+            Assert.True(heap.HeapArray[1] == 10);
+            Assert.True(heap.HeapArray[2] == 11);
+            Assert.True(heap.HeapArray[3] == 5);
+        }
+
+        private void Check(Heap heap, int[] ints)
+        {
+            foreach (var key in ints)
+            {
+                Assert.True(heap.GetMax() == key);
+            }
+        }
+    }
+}
