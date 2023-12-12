a790858af3c66c84dfd28ab69afd14aa4441373f
Alexey Esin <a.esin@kefirgames.com>
2023-12-12 20:05:04 +0300

    ADS2 08

---
+++ ADS2/08/08/SimpleGraph.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class Vertex
+    {
+        public int Value;
+
+        public Vertex(int val)
+        {
+            Value = val;
+        }
+    }
+
+    public class SimpleGraph
+    {
+        public Vertex[] vertex;
+        public int[,] m_adjacency;
+        public int max_vertex;
+
+        public SimpleGraph(int size)
+        {
+            max_vertex = size;
+            m_adjacency = new int [size, size];
+            vertex = new Vertex [size];
+        }
+
+        public void AddVertex(int value)
+        {
+            var index = GetFreeIndex();
+            vertex[index] = new Vertex(value);
+        }
+
+        private int GetFreeIndex()
+        {
+            for (int i = 0; i < max_vertex; i++)
+            {
+                if (vertex[i] == null)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        public void RemoveVertex(int v)
+        {
+            for (var i = 0; i < max_vertex; i++)
+            {
+                m_adjacency[v, i] = 0;
+                m_adjacency[i, v] = 0;
+            }
+
+            vertex[v] = null;
+        }
+
+        public bool IsEdge(int v1, int v2)
+        {
+            return m_adjacency[v1, v2] == 1;
+        }
+
+        public void AddEdge(int v1, int v2)
+        {
+            m_adjacency[v1, v2] = 1;
+            m_adjacency[v2, v1] = 1;
+        }
+
+        public void RemoveEdge(int v1, int v2)
+        {
+            m_adjacency[v1, v2] = 0;
+            m_adjacency[v2, v1] = 0;
+        }
+    }
+}
---
+++ ADS2/08/08/Tests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.ComponentModel.Design;
+using AlgorithmsDataStructures2;
+using NUnit.Framework;
+
+namespace _08
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestAdd()
+        {
+            var t = new SimpleGraph(3);
+            t.AddVertex(1);
+            t.AddVertex(2);
+            t.AddVertex(3);
+            Check(t, new[] {1, 2, 3}, new[] {0, 0, 0, 0, 0, 0, 0, 0, 0});
+        }
+
+        [Test]
+        public void TestEdges()
+        {
+            var t = new SimpleGraph(3);
+            t.AddVertex(1);
+            t.AddVertex(2);
+            t.AddVertex(3);
+            t.AddEdge(0, 1);
+            t.AddEdge(1, 2);
+            Check(t, new[] {1, 2, 3}, new[] {0, 1, 0, 1, 0, 1, 0, 1, 0});
+            t.AddEdge(2, 0);
+            Check(t, new[] {1, 2, 3}, new[] {0, 1, 1, 1, 0, 1, 1, 1, 0});
+            t.AddEdge(2, 0);
+            Check(t, new[] {1, 2, 3}, new[] {0, 1, 1, 1, 0, 1, 1, 1, 0});
+        }
+
+        [Test]
+        public void TestEdges2()
+        {
+            var t = new SimpleGraph(3);
+            t.AddVertex(1);
+            t.AddVertex(2);
+            t.AddVertex(3);
+            t.AddEdge(0, 1);
+            t.AddEdge(1, 2);
+            Check(t, new[] {1, 2, 3}, new[] {0, 1, 0, 1, 0, 1, 0, 1, 0});
+            t.AddEdge(2, 0);
+            Check(t, new[] {1, 2, 3}, new[] {0, 1, 1, 1, 0, 1, 1, 1, 0});
+            t.AddEdge(2, 0);
+            Check(t, new[] {1, 2, 3}, new[] {0, 1, 1, 1, 0, 1, 1, 1, 0});
+        }
+        
+        [Test]
+        public void TestRemove()
+        {
+            var t = new SimpleGraph(3);
+            t.AddVertex(1);
+            t.AddVertex(2);
+            t.AddVertex(3);
+            t.AddEdge(0, 1);
+            t.AddEdge(1, 2);
+            t.AddEdge(2, 0);
+            t.RemoveEdge(2, 0);
+            Check(t, new[] {1, 2, 3}, new[] {0, 1, 0, 1, 0, 1, 0, 1, 0});
+            t.RemoveEdge(1, 0);
+            Assert.False(t.IsEdge(1,0));
+            Assert.False(t.IsEdge(0,1));
+            t.RemoveEdge(1, 2);
+            Assert.False(t.IsEdge(1,2));
+            Assert.False(t.IsEdge(2,1));
+            Check(t, new[] {1, 2, 3}, new[] {0, 0, 0, 0, 0, 0, 0, 0, 0});
+        }
+        
+        [Test]
+        public void TestRemove2()
+        {
+            var t = new SimpleGraph(3);
+            t.AddVertex(1);
+            t.AddVertex(2);
+            t.AddVertex(3);
+            t.AddEdge(0, 1);
+            t.AddEdge(1, 2);
+            t.RemoveVertex(1);
+            Assert.False(t.IsEdge(1,0));
+            Assert.False(t.IsEdge(0,1));
+            Assert.False(t.IsEdge(1,2));
+            Assert.False(t.IsEdge(2,1));
+            
+            Check(t, new[] {1, -1, 3}, new[] {0, 0, 0, 0, 0, 0, 0, 0, 0});
+            t.AddVertex(4);
+            Check(t, new[] {1, 4, 3}, new[] {0, 0, 0, 0, 0, 0, 0, 0, 0});
+        }
+
+
+        private void Check(SimpleGraph simpleGraph, int[] val, int[] edgs)
+        {
+            for (var i = 0; i < val.Length; i++)
+            {
+                if (val[i] == -1)
+                {
+                    Assert.True(simpleGraph.vertex[i] == null);
+                }
+                else
+                {
+                    Assert.True(simpleGraph.vertex[i].Value == val[i]);
+                }
+            }
+
+            for (var i = 0; i < simpleGraph.max_vertex; i++)
+            {
+                for (var j = 0; j < simpleGraph.max_vertex; j++)
+                {
+                    Assert.True(simpleGraph.IsEdge(i, j) == simpleGraph.IsEdge(j, i));
+                    Assert.True((simpleGraph.IsEdge(i, j) ? 1 : 0) == edgs[i * simpleGraph.max_vertex + j]);
+                    Assert.True(simpleGraph.m_adjacency[i, j] == edgs[i * simpleGraph.max_vertex + j]);
+                }
+            }
+        }
+    }
+}
